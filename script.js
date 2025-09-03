348ae7ca61ca44413f2a2dd40cf79fa91a83553a
root <root@devops-vm>
2025-09-03 12:06:38 +0000

    Updated site with React portfolio

--- script.js
+++
@@ -1,5 +0,0 @@
-// Small scroll effect for navbar
-window.addEventListener("scroll", () => {
-  const nav = document.querySelector("nav");
-  nav.classList.toggle("sticky", window.scrollY > 0);
-});
